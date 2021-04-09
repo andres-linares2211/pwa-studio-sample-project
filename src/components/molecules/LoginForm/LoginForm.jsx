a6c9da1c35f33b236c4a33d92fd63a96c6cf8607
andres-linares2211 <andres.linares@omni.pro>
2021-04-09 12:39:28 -0500

    feat: styles for login page

--- src/components/molecules/LoginForm/LoginForm.jsx
+++ src/components/molecules/LoginForm/LoginForm.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import Button from '@magento/venia-ui/lib/components/Button';
 import Field from '@magento/venia-ui/lib/components/Field';
+import TextInput from '@magento/venia-ui/lib/components/TextInput';
+import Password from '@magento/venia-ui/lib/components/Password';
 import LinkButton from '@magento/venia-ui/lib/components/LinkButton';
+import { isRequired } from '@magento/venia-ui/lib/util/formValidators';
 
 import classes from './LoginForm.css';
 
 const LoginForm = () => {
+  const { formatMessage } = useIntl();
+
   return (
     <form className={classes.root}>
-      <Field label="Email">
-        <input />
+      <Field
+        label={formatMessage({
+          id: 'signIn.emailAddressText'
+        })}
+      >
+        <TextInput autoComplete="email" field="email" validate={isRequired} />
       </Field>
 
-      <Field label="Password">
-        <input />
-      </Field>
+      <Password
+        fieldName="password"
+        label={formatMessage({
+          id: 'signIn.passwordText'
+        })}
+        validate={isRequired}
+        autoComplete="current-password"
+        isToggleButtonHidden={false}
+      />
 
-      <Button priority="high">
+      <Button priority="high" type="submit">
         <FormattedMessage id="global.logIn" />
       </Button>
 
