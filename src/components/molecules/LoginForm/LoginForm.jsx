305b5bca24f79a07642274c21a32a1b5b4c287a3
andres-linares2211 <andres.linares@omni.pro>
2021-04-09 12:58:10 -0500

    feat: functional login

--- src/components/molecules/LoginForm/LoginForm.jsx
+++ src/components/molecules/LoginForm/LoginForm.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
+import { Form } from 'informed';
 
 import Button from '@magento/venia-ui/lib/components/Button';
 import Field from '@magento/venia-ui/lib/components/Field';
 import TextInput from '@magento/venia-ui/lib/components/TextInput';
 import Password from '@magento/venia-ui/lib/components/Password';
 import LinkButton from '@magento/venia-ui/lib/components/LinkButton';
+import FormError from '@magento/venia-ui/lib/components/FormError';
 import { isRequired } from '@magento/venia-ui/lib/util/formValidators';
+import { GET_CART_DETAILS_QUERY } from '@magento/venia-ui/lib/components/SignIn/signIn.gql';
+import { useSignIn } from '@magento/peregrine/lib/talons/SignIn/useSignIn';
 
 import classes from './LoginForm.css';
 
 const LoginForm = () => {
   const { formatMessage } = useIntl();
+  const talonProps = useSignIn({
+    getCartDetailsQuery: GET_CART_DETAILS_QUERY,
+    setDefaultUsername: () => {},
+    showCreateAccount: () => {},
+    showForgotPassword: () => {}
+  });
+
+  const { errors, handleSubmit, isBusy, setFormApi } = talonProps;
 
   return (
-    <form className={classes.root}>
+    <Form className={classes.root} getApi={setFormApi} onSubmit={handleSubmit}>
+      <FormError errors={Array.from(errors.values())} />
+
       <Field
         label={formatMessage({
           id: 'signIn.emailAddressText'
         })}
       >
-        <TextInput autoComplete="email" field="email" validate={isRequired} />
+        <TextInput autoComplete="email" field="email" validate={isRequired} disabled={isBusy} />
       </Field>
 
       <Password
@@ -31,16 +45,17 @@ const LoginForm = () => {
         validate={isRequired}
         autoComplete="current-password"
         isToggleButtonHidden={false}
+        disabled={isBusy}
       />
 
-      <Button priority="high" type="submit">
+      <Button priority="high" type="submit" disabled={isBusy}>
         <FormattedMessage id="global.logIn" />
       </Button>
 
       <LinkButton type="button">
         <FormattedMessage id="global.passwordForget" />
       </LinkButton>
-    </form>
+    </Form>
   );
 };
 
