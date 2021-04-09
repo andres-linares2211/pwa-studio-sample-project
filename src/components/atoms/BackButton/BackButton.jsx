f61570ee52cfbe5cdc8a5e1486683d369d6b51a6
andres-linares2211 <andres.linares@omni.pro>
2021-04-09 10:10:55 -0500

    feat: back button goes back on click

--- src/components/atoms/BackButton/BackButton.jsx
+++ src/components/atoms/BackButton/BackButton.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
 import IconBack from '../../icons/IconBack';
 
 const BackButton = () => {
+  const history = useHistory();
+
+  const goBack = () => history.goBack();
+
   return (
-    <button>
+    <button onClick={goBack}>
       <IconBack />
       <FormattedMessage id="{'Back'}" />
     </button>
